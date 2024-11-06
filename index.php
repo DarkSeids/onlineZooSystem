33a03268edaf6e6f92c051313dd00a12801156db
DarkSeids <giripurushotam39@gmail.com>
2024-11-06 18:40:31 +0545

    added some code

--- index.php
+++ index.php
@@ -1,54 +1,54 @@
  <?php include "includes/db.php"; ?>
-<?php include "includes/header.php"; ?>
-    
-    <!-- Navigation --> 
-    <?php include "includes/navigation.php"; ?>
+ <?php include "includes/header.php"; ?>
 
-    <div class="container">
+ <!-- Navigation -->
+ <?php include "includes/navigation.php"; ?>
 
-        <div class="row">
+ <div class="container">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+   <div class="row">
 
-                 <div class="container" style="width: 40%;">
+     <!-- Blog Entries Column -->
+     <div class="col-md-8">
 
-                            <h2 style="margin-left: 37%;">Welcome To</h2>
+       <div class="container" style="width: 40%;">
 
-      <img src="images/claybrook.jpg" width="300" style="margin-left: 13%;" class="img-circle" alt="Profile"> <br> <br> <br>
+         <h2 style="margin-left: 37%;">Welcome To</h2>
 
-      </div>
-      <div style="col-md-4">
-        <h4 style="color: #910606">See Our Catalog</h4>
+         <img src="images/claybrook.jpg" width="300" style="margin-left: 13%;" class="img-circle" alt="Profile"> <br> <br> <br>
+
+       </div>
+       <div style="col-md-4">
+         <h4 style="color: #910606">See Our Whole Catalog</h4>
 
 
-           <li>
-             <p>Birds</p>
+         <li>
+           <p>Birds</p>
            <img width="200" height="200;" style="margin-right: 10%;" src="gallery/birds.jpg" alt="">
-            <br> <br>
-             <p>Fish</p>
-            <img width="200" height="200;" style="margin-right: 10%;" src="gallery/fishes.jpg" alt="">
-            <br> <br>
-             <p>Reptiles</p>
-             <img width="200" height="200;" style="margin-right: 10%;" src="gallery/reptiles.jpg" alt="">
-            <br> <br>
-            <p>Amphibian</p>
-            <img width="200" height="200;" style="margin-right: 10%;" src="gallery/amphibian.jpg" alt="">
-           
-            </li>
+           <br> <br>
+           <p>Fish</p>
+           <img width="200" height="200;" style="margin-right: 10%;" src="gallery/fishes.jpg" alt="">
+           <br> <br>
+           <p>Reptiles</p>
+           <img width="200" height="200;" style="margin-right: 10%;" src="gallery/reptiles.jpg" alt="">
+           <br> <br>
+           <p>Amphibian</p>
+           <img width="200" height="200;" style="margin-right: 10%;" src="gallery/amphibian.jpg" alt="">
 
-                 </div>
+         </li>
 
-                 </div>
+       </div>
 
-
-            <!-- Blog Sidebar Widgets Column -->
-            <?php include "includes/sidebar.php"; ?>
-
-        </div>
-        <!-- /.row -->
+     </div>
 
 
+     <!-- Blog Sidebar Widgets Column -->
+     <?php include "includes/sidebar.php"; ?>
 
- 
-<?php include "includes/footer.php"; ?>
+   </div>
+   <!-- /.row -->
+
+
+
+
+   <?php include "includes/footer.php"; ?>
